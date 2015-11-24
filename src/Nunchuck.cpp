55c7ee5c7457005773b42c9d971c719201110f09
Tim Potze <timpotze@gmail.com>
2015-11-24 22:18:20 +0100

    Added comments to nunchuck class and main

--- src/Nunchuck.cpp
+++ src/Nunchuck.cpp
@@ -21,72 +21,98 @@ bool Nunchuck::begin() {
     DDRC |= _BV(PINC3) | _BV(PINC2);
     PORTC &= ~_BV(PINC2);
     PORTC |= _BV(PINC3);
+
+    // give connected devices some time to boot.
     _delay_ms(50);
 
+    // enable the two wire interface.
     Wire.begin();
 
+    // initialize the connected device.
     return init();
 }
 
 bool Nunchuck::init() {
-    // request id
+    // request id of the device.
     Wire.beginTransmission(NC_ADDRESS);
-    Wire.write((uint8_t)0xFA);
+    Wire.write((uint8_t) 0xFA);
     Wire.endTransmission();
 
+    // check which type of device has been connected by checking the length of the returned identifier.
+    // old nunchucks do not return an identifier, new nunchucks do return an identifier of 6 bytes.
     type_ = clear_input() < 6 ? 1 : 2;
-    // initialize registers
+
+    // set the initialize registers of the nunchuck to their respective value.
     Wire.beginTransmission(NC_ADDRESS);
-    Wire.write((uint8_t) NC1_INIT_REGISTER);
-    Wire.write((uint8_t) NC1_INIT_DATA);
+    if (type_ == 1) {
+        Wire.write((uint8_t) NC1_INIT_REGISTER);
+        Wire.write((uint8_t) NC1_INIT_DATA);
+    }
+    else {
+        Wire.write((uint8_t) NC2_INIT_REGISTER);
+        Wire.write((uint8_t) NC2_INIT_DATA);
+    }
     Wire.endTransmission();
 
-    Wire.beginTransmission(NC_ADDRESS);
-    Wire.write((uint8_t) NC2_INIT_REGISTER);
-    Wire.write((uint8_t) NC2_INIT_DATA);
-    Wire.endTransmission();
-
+    // clear remaining data on the wire input buffer.
     clear_input();
 
+    // send a request for data about the state of the device.
     send_request();
-    if(!retrieve_data()) {
+
+    // attempt to store the received data to the buffer. if data can not be received, assume the device has been
+    // disconnected and reset the type to 0.
+    if (!receive_data()) {
         type_ = 0;
     }
+
+    // send a new request for data ahead for the first update.
     send_request();
 
     return true;
 }
 
 void Nunchuck::update() {
-    if(type_ <= 0 && !init()) return;
+    // if no device has been connected and the initialisation method failed, do not proceed with the update.
+    if (type_ <= 0 && !init()) return;
 
-    if(!retrieve_data()) {
+    // attempt to store the received data to the buffer. if data can not be received, assume the device has been
+    // disconnected and reset the type to 0.
+    if (!receive_data()) {
         type_ = 0;
     }
 
+    // send a new request for data ahead for the next update.
     send_request();
 }
 
 void Nunchuck::send_request() {
+    // send a request byte to the device.
     Wire.beginTransmission(NC_ADDRESS);
     Wire.write((uint8_t) NC_REQUEST);
     Wire.endTransmission();
 }
 
-bool Nunchuck::retrieve_data() {
+bool Nunchuck::receive_data() {
+    // request 6 bytes from the device to write to the buffer.
     Wire.requestFrom(NC_ADDRESS, 6);
 
     uint8_t i = 0;
     for (; i < 6 && Wire.available(); i++) {
         uint8_t raw = (uint8_t) Wire.read();
+
+        // write the read value to the buffer. if we're dealing with an old nunchuck, decode the value first.
         buffer_[i] = type_ == 1 ? ((raw ^ 0x17) + 0x17) : raw;
     }
 
+    // clear remaining data on the wire input buffer.
     clear_input();
     return i == 6;
 }
 
 Vector3 Nunchuck::acceleration() {
+    // if no device has been connected, return an empty vector. otherwise, return a vector filled with acceleration
+    // data.
     return !type_ ? Vector3() : Vector3(
             range_from_10bits(buffer_[2], (buffer_[5] >> 2) & 3, 0, 1023),
             range_from_10bits(buffer_[3], (buffer_[5] >> 4) & 3, 0, 1023),
@@ -95,6 +121,7 @@ Vector3 Nunchuck::acceleration() {
 }
 
 Vector2 Nunchuck::joystick() {
+    // if no device has been connected, return an empty vector. otherwise, return a vector filled with joystick data.
     return !type_ ? Vector2() : Vector2(
             range_from_8bits(buffer_[0], 0, 255),
             range_from_8bits(buffer_[1], 0, 255)
@@ -102,14 +129,17 @@ Vector2 Nunchuck::joystick() {
 }
 
 bool Nunchuck::button_z() {
+    // return a value indicating whether any device has been connected and the z button has been pressed.
     return type_ && !((buffer_[5] >> 0) & 1);
 }
 
 bool Nunchuck::button_c() {
+    // return a value indicating whether any device has been connected and the c button has been pressed.
     return type_ && !((buffer_[5] >> 1) & 1);
 }
 
 float Nunchuck::range_from_10bits(uint8_t byte, uint8_t bits, uint16_t min, uint16_t max) {
+    // return a number between -1 and 1 based on the specified 10 bit number and range.
     uint16_t value = (byte << 2) | (bits & 3);
     value -= min;
     max -= min;
@@ -117,6 +147,7 @@ float Nunchuck::range_from_10bits(uint8_t byte, uint8_t bits, uint16_t min, uint
 }
 
 float Nunchuck::range_from_8bits(uint8_t byte, uint8_t min, uint8_t max) {
+    // return a number between -1 and 1 based on the specified 8 bit number and range.
     byte -= min;
     max -= min;
     return ((float) byte / max) * 2 - 1;
@@ -127,10 +158,11 @@ uint8_t Nunchuck::type() {
 }
 
 uint8_t Nunchuck::clear_input() {
+    // Clear any remaining bytes in the wire buffer and return the number of bytes cleared from the buffer.
     uint8_t count;
 
     Wire.requestFrom(NC_ADDRESS, 128);
-    for(count = 0; Wire.available();count++)
+    for (count = 0; Wire.available(); count++)
         Wire.read();
 
     return count;
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,54 @@
 #include <Arduino.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <digitalWriteFast.h>
 #include <MI0283QT9.h>
-
 #include "Nunchuck.h"
 #include "Game.h"
+
+// include some header files of libraries used by various libraries.
+// this triggers platform.io to load the packages they are a part of.
+#include <SPI.h>
+#include <digitalWriteFast.h>
 
 Game *game = 0;
 MI0283QT9 tft;
 Nunchuck nc;
 
-void enableInterrupt() {
-    TCCR2B = 0x00;
-    TCCR2A &= ~((1 << COM0A0) | (1 << COM0A1));
+void enableTimer2Interrupt() {
+    // set the prescaler to 32 and enable the interrupt.
     TCCR2B = 0 << CS22 | 1 << CS21 | 1 << CS20;
     TIMSK2 |= 1 << TOIE2;
 
+    // enable interrupts
     sei();
 }
 
 ISR(TIMER2_OVF_vect) {
-    if(game)
-        game->tick((float)1 / ((float)16000000 / 256 / 64));
+    // if the game has been initialized, advance it's internal timers.
+    if (game)
+        game->tick((float) 1 / ((float) 16000000 / 256 / 32 / 2));
 }
 
-int main(void) {
-    init();
-    enableInterrupt();
 
+int main(void) {
+    // some arduino functions are still used throughout the code.
+    // let arduino do some initializing.
+    init();
+
+    // enable the timer 2 interrupt.
+    enableTimer2Interrupt();
+
+    // initialize the tft screen and clear it to it's initial color (red).
     tft.begin();
     tft.led(100);
     tft.fillScreen(RGB(255, 0, 0));
 
+    // initialize the nunchuck interface.
     nc.begin();
 
+    // create an instance of the game.
     game = new Game(&nc, &tft);
 
-    for( ;;) {
+    // keep updating the game until the end of time (or the power is cut).
+    for (; ;) {
         game->update();
     }
 }
