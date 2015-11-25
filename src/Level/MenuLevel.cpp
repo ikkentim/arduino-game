20363c0f00ab03b7d2e83c5723b943f3a01ec94e
Aron van Sleen <aron.vansleen@gmail.com>
2015-11-25 13:39:33 +0000

    Removed red background from main and cleaned up MenuLevel a tad

--- src/Level/MenuLevel.cpp
+++ src/Level/MenuLevel.cpp
@@ -8,12 +8,14 @@ MenuLevel::MenuLevel(Game *game) : Level(game) {
 }
 
 void MenuLevel::render(MI0283QT9 *tft) {
+    const char *text = "Touch the screen to continue";
     const uint8_t title_size = 4;
 
     // Render the coolest title in the world (Math to center it horizontally, the 8 is because of 8 bits)
     tft->drawText((tft->lcd_width / 2) - (((float) strlen(menu_title_) / 2) * 8 * title_size), tft->lcd_height / 4,
-                  menu_title_, RGB(rand() % 255, rand() % 255, rand() % 255), RGB(0, 0, 0), title_size);
-
+                  menu_title_, RGB(rand() % 200 + 55, rand() % 200 + 55, rand() % 200 + 55), RGB(0, 0, 0), title_size);
+    tft->drawText((tft->lcd_width / 2) - (((float) strlen(text) / 2) * 8), tft->lcd_height - 20, text,
+                  RGB(255, 255, 255), RGB(0, 0, 0), 1);
 }
 
 void MenuLevel::update() {
