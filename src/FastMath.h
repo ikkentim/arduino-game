806e8c00f6fbe4647902c113a888337e4f78b742
Luuk Holleman <luukholleman@gmail.com>
2015-11-25 15:48:16 +0100

    Added fast math

---
+++ src/FastMath.h
@@ -0,0 +1,11 @@
+#ifndef ARDUINOGAME_FASTMATH_H
+#define ARDUINOGAME_FASTMATH_H
+
+float fast_sin(float x, bool lessPrecision = false);
+
+float fast_cos(float x, bool lessPrecision = false);
+
+float fast_sqrt(float x);
+
+
+#endif //ARDUINOGAME_FASTMATH_H
