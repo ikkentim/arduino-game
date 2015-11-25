eb30ff72fe6f9ca15fb909cd83cbdb5067a78550
Roelof <snijder15@gmail.com>
2015-11-25 14:41:07 +0100

    BaseGame entity added and created testobject, ShapeRenderer is not implemented in this version, when it is implemented include it in the CPP files.

--- src/Level/TestLevel.cpp
+++ src/Level/TestLevel.cpp
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "TestLevel.h"
-
+#include "../Entity/TestEntity.h"
 void TestLevel::render(MI0283QT9 *tft) {
 
 }
 
 TestLevel::TestLevel(Game *game) : Level(game) {
-
+    testEntity_ = new TestEntity(game);
 }
 
+TestLevel::~TestLevel()
+{
+    delete testEntity_;
+}
 void TestLevel::update() {
-
+    testEntity_->update(0.16f); //TODO: add delta time to Level::update
 }
+
