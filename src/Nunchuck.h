285296e6b255e8ada5bf85a40499cfaf32ade4b3
Luuk Holleman <luukholleman@gmail.com>
2015-11-30 12:29:44 +0100

    Calibrate nunchuck input

--- src/Nunchuck.h
+++ src/Nunchuck.h
@@ -28,8 +28,15 @@ private:
     bool receive_data();
     float range_from_10bits(uint8_t byte, uint8_t bits, uint16_t min, uint16_t max);
     float range_from_8bits(uint8_t byte, uint8_t min, uint8_t max);
+    void wrap(Vector2 *vector);
+    void wrap(Vector3 *vector);
     uint8_t type_; // 0 - disconnected, 1 - old, 2 - new
     uint8_t buffer_[8];
+
+    Vector2 position_;
+    Vector3 acceleration_;
+    bool z_pressed_;
+    bool c_pressed_;
 };
 
 
