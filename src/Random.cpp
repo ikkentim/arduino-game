fc8947590c5d473f7d1fc4b3a2a6d5365e543a59
Roelof <snijder15@gmail.com>
2015-12-01 14:20:12 +0000

    Added comments to generate_seed per request of the reviewer.

--- src/Random.cpp
+++ src/Random.cpp
@@ -6,6 +6,7 @@ unsigned int Random::seed_ = 0;
 
 void Random::generate_seed(uint8_t ticks, Nunchuck *nunchuck) {
     //int is 16 bit
+	//prime numbers for generation of the seed
     const unsigned int p1 = 45013;
     const unsigned int p2 = 34429;
     const unsigned int p3 = 54421;
@@ -21,9 +22,9 @@ void Random::generate_seed(uint8_t ticks, Nunchuck *nunchuck) {
                    aY * p2 +
                    aZ * p3 +
                    ticks * ticks) % p4);
-
+	//Add the seed to the _seed value so that it overflows
     Random::seed_ += seed;
-    srand(Random::seed_);
+    srand(Random::seed_); //Set the rand seed.
 }
 
 int Random::rand_integer(const int &range_min, const int &range_max) {
