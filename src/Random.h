a75d3d4fc5219f81c6e5d844bc11f1a2a88f6bdd
Aron van Sleen <aron.vansleen@gmail.com>
2015-12-01 14:11:50 +0000

    Did some reformatting

--- src/Random.h
+++ src/Random.h
@@ -2,16 +2,15 @@
 #define ARDUINOGAME_RANDOM_H
 
 #include <stdint.h>
+
 class MenuLevel;
 class Nunchuck;
 
-class Random
-{
+class Random {
 public:
     int rand_integer(const int& range_min, const int& range_max = RAND_MAX);
     uint8_t rand_uint8_t(const uint8_t& range_min, const uint8_t& range_max = 255);
     float rand_float(const float& range_min, const float& range_max = RAND_MAX);
-
     int get_seed() const;
 private:
     //The seed for the random generator
