035ed2720fe3010fb502e08a871ce197f4291e5d
Tim Potze <timpotze@gmail.com>
2015-12-11 13:38:54 +0100

    Optimized ShapeRenderer, added shape scale

--- src/ShapeRenderer.cpp
+++ src/ShapeRenderer.cpp
@@ -1,10 +1,6 @@
 #include "ShapeRenderer.h"
-#include "Matrix4.h"
-#include "Vector2.h"
 #include <avr/pgmspace.h>
-#include <util/delay.h>
 #include "FastMath.h"
-#include <math.h>
 
 #define IN_RANGE(value, min, max) (value >= min && value < max) //inclusive min, exclusive max
 #define SWAP(x, y) do{ (x)=(x)^(y); (y)=(x)^(y); (x)=(x)^(y); }while(0)
@@ -12,29 +8,51 @@
 #define BUFFER_SIZE     (32)
 
 void ShapeRenderer::render(TFT *tft, int8_t *shape, int length, Color color, int oldx, int oldy, float oldr,
-                           int newx, int newy, float newr) {
+                           int newx, int newy, float newr, float scale) {
+    if (scale > 1) scale = 1;
+    if (scale < 0.1f) scale = 0.1f;
+
     // Clear the buffer.
     buffer_clear(buffer1_);
 
     // Pre-calculate sine and cosine of the new rotation.
     float
-        sine = fast_sin(newr),
-        cosine = fast_cos(newr);
+        sine = newr == 0 ? 0.0f : fast_sin(newr),
+        cosine = newr == 0 ? 1.0f : fast_cos(newr);
 
     // Loop trough every line in shape.
     for (int j = 0, i = 0; i < length; i++) {
         // Read line coordinates from the program memory.
-        int8_t
-            lineStartX = (int8_t) pgm_read_byte(shape + j++),
-            lineStartY = (int8_t) pgm_read_byte(shape + j++),
-            lineEndX = (int8_t) pgm_read_byte(shape + j++),
+        float lineStartX, lineStartY, lineEndX, lineEndY;
+
+        if (scale == 1) {
+            lineStartX = (int8_t) pgm_read_byte(shape + j++);
+            lineStartY = (int8_t) pgm_read_byte(shape + j++);
+            lineEndX = (int8_t) pgm_read_byte(shape + j++);
             lineEndY = (int8_t) pgm_read_byte(shape + j++);
+        }
+        else {
+            lineStartX = (int8_t) pgm_read_byte(shape + j++) * scale;
+            lineStartY = (int8_t) pgm_read_byte(shape + j++) * scale;
+            lineEndX = (int8_t) pgm_read_byte(shape + j++) * scale;
+            lineEndY = (int8_t) pgm_read_byte(shape + j++) * scale;
+        }
 
         // Rotate the line coordinates based on pre-calculated sine/cosine.
-        int startX = (int) (cosine * lineStartX - sine * lineStartY),
-            startY = (int) (sine * lineStartX + cosine * lineStartY),
-            endX = (int) (cosine * lineEndX - sine * lineEndY),
+        int startX, startY, endX, endY;
+
+        if (newr == 0) {
+            startX = (int) lineStartX;
+            startY = (int) lineStartY;
+            endX = (int) lineEndX;
+            endY = (int) lineEndY;
+        }
+        else {
+            startX = (int) (cosine * lineStartX - sine * lineStartY);
+            startY = (int) (sine * lineStartX + cosine * lineStartY);
+            endX = (int) (cosine * lineEndX - sine * lineEndY);
             endY = (int) (sine * lineEndX + cosine * lineEndY);
+        }
 
         // Draw the line on the buffer.
         buffer_draw_line(buffer1_, startX + BUFFER_SIZE / 2, startY + BUFFER_SIZE / 2, endX + BUFFER_SIZE / 2,
@@ -51,17 +69,36 @@ void ShapeRenderer::render(TFT *tft, int8_t *shape, int length, Color color, int
     // Loop trough every line in shape.
     for (int j = 0, i = 0; i < length; i++) {
         // Read line coordinates from the program memory.
-        int8_t
-            lineStartX = (int8_t) pgm_read_byte(shape + j++),
-            lineStartY = (int8_t) pgm_read_byte(shape + j++),
-            lineEndX = (int8_t) pgm_read_byte(shape + j++),
+        float lineStartX, lineStartY, lineEndX, lineEndY;
+
+        if (scale == 1) {
+            lineStartX = (int8_t) pgm_read_byte(shape + j++);
+            lineStartY = (int8_t) pgm_read_byte(shape + j++);
+            lineEndX = (int8_t) pgm_read_byte(shape + j++);
             lineEndY = (int8_t) pgm_read_byte(shape + j++);
+        }
+        else {
+            lineStartX = (int8_t) pgm_read_byte(shape + j++) * scale;
+            lineStartY = (int8_t) pgm_read_byte(shape + j++) * scale;
+            lineEndX = (int8_t) pgm_read_byte(shape + j++) * scale;
+            lineEndY = (int8_t) pgm_read_byte(shape + j++) * scale;
+        }
 
         // Rotate the line coordinates based on pre-calculated sine/cosine.
-        int startX = (int) (cosine * lineStartX - sine * lineStartY),
-            startY = (int) (sine * lineStartX + cosine * lineStartY),
-            endX = (int) (cosine * lineEndX - sine * lineEndY),
+        int startX, startY, endX, endY;
+
+        if (oldr == 0) {
+            startX = (int)lineStartX;
+            startY = (int)lineStartY;
+            endX = (int)lineEndX;
+            endY = (int)lineEndY;
+        }
+        else {
+            startX = (int) (cosine * lineStartX - sine * lineStartY);
+            startY = (int) (sine * lineStartX + cosine * lineStartY);
+            endX = (int) (cosine * lineEndX - sine * lineEndY);
             endY = (int) (sine * lineEndX + cosine * lineEndY);
+        }
 
         // Loop trough every pixel in the line.
         int16_t
@@ -269,6 +306,6 @@ uint8_t ShapeRenderer::buffer_get_pixel(uint32_t *buffer, int x, int y) {
 }
 
 void ShapeRenderer::buffer_clear(uint32_t *buffer) {
-    for(uint8_t i=0;i<32;i++)
-        buffer[i]=0;
+    for (uint8_t i = 0; i < 32; i++)
+        buffer[i] = 0;
 }
